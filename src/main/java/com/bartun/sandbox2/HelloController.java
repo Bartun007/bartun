dbe2cd252c0fb13830947c46b69df1ceb06ff3f3
Bartlomiej Adamczyk <badamczy@akamai.com>
2026-02-02 09:58:13 +0100

    abc

--- src/main/java/com/bartun/sandbox2/HelloController.java
+++ src/main/java/com/bartun/sandbox2/HelloController.java
@@ -1,14 +1,20 @@
 package com.bartun.sandbox2;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello, world";
+    public Map<String,Object> hello(HttpServletRequest request) {
+
+      Map<String, Object> result = Map.of("version", "0.1", "clientIp", request.getRemoteAddr());
+
+      return result;
     }
 }
 
